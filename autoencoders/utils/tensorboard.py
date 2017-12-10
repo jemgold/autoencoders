580f65867f5c0c174a359988546bd80fb4e2d0fd
Jon Gold <hello@designedbygold.com>
2017-12-09 16:17:20 -0800

    Tensorboard run_path should return something

--- autoencoders/utils/tensorboard.py
+++ autoencoders/utils/tensorboard.py
@@ -6,4 +6,5 @@ def run_path(exp_name):
     Format a tensorboard run name
     """
 
-    'runs/{}_{}'.format(exp_name, datetime.now().strftime('%b%d_%H-%M-%S'))
+    return 'runs/{}_{}'.format(exp_name,
+                               datetime.now().strftime('%b%d_%H-%M-%S'))
