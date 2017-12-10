f1f75b8caa2ea822068fca7b0b2d242ae279ca31
Jon Gold <hello@designedbygold.com>
2017-12-09 17:36:28 -0800

    fix typo in experiment name

--- autoencoders/models/ConditionalVariationalAutoencoder.py
+++ autoencoders/models/ConditionalVariationalAutoencoder.py
@@ -69,7 +69,7 @@ def train(epochs=10, batch_size=64, latent_dim=2,
     from autoencoders.models.loss import VAELoss
 
     experiment_name = run_path(
-        'cvae_l{}_h{}_adam_3e-4'.format(latent_dim, batch_size))
+        'cvae_l{}_h{}_adam_3e-4'.format(latent_dim, hidden_dim))
 
     writer = SummaryWriter(experiment_name)
 
